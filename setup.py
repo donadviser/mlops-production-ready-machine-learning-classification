b27df5e81628e2ef76327d2264a10b0431392881
Derrick Njobuenwu <donadviser@gmail.com>
2024-09-24 15:08:39 +0100

    add requirement and step up

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "usvisa"
+AUTHOR_USER_NAME = "donadviser"
+SRC_REPO = "usvisa"
+AUTHOR_EMAIL = "donadviser@gmail.com"
+
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A production-grade machine learning classification for US Visa Application prediction using AWS services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    packages=setuptools.find_packages(where=REPO_NAME),
+    package_dir={"": REPO_NAME},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.9",
+)
